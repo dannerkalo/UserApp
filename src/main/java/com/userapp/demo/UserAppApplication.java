6deb80fa5a23545897084dffe3e2ddd1b2b7fe0d
Dannerkalo <Dannerkalo@gmail.com>
2019-12-22 22:37:26 +0100

    Front-page with ThymeLeaf and Restcontroller replaced with MVC.

--- src/main/java/com/userapp/demo/UserAppApplication.java
+++ src/main/java/com/userapp/demo/UserAppApplication.java
@@ -2,12 +2,22 @@ package com.userapp.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication 
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"import com.userapp.demo"})
+@EnableJpaRepositories(basePackages="com.userapp.demo.repositories")
+@EnableTransactionManagement
+@EntityScan(basePackages="com.userapp.demo.models")
 public class UserAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserAppApplication.class, args);
+    }
+    
 }
---
+++ src/main/java/com/userapp/demo/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.userapp.demo.repositories;
+
+import com.userapp.demo.models.User;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+    List<User> findByName(String name);
+    
+}
